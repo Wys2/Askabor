aef5c9a91f61ce8d7dc0c82dfeb2c9a54d463091
Wys2 <noahgaming951@gmail.com>
2022-03-02 21:02:23 +0100

    Modification du startup

--- src/askabor.js
+++ src/askabor.js
@@ -1,34 +1,23 @@
+// Code by Wys#0001 || https://github.com/Wys2
+
+// Variables globales
+const functions = require("./assets/functions.js")
+const fs = require('fs');
+const chalk = require("chalk")
+const pupeteer = require("puppeteer")
+
+// Startup
 var args = process.argv.slice(2);
-if(args[0] == "-test"){
+if(args[0] == "-t"){
   console.log("Debug mode activated")
 }
-console.clear()
-console.log("                                                      .                         ")
-console.log("                                                   ...                          ")
-console.log("                                                   ..                           ")
-console.log("                                            ..     ...                          ")
-console.log("                             ........ .                                         ")
-console.log("                     ..         .            .      .       .                   ")
-console.log("                     ,.            . . ..     ... ..                            ")
-console.log("                    ..            ..... ,. . .  . .    ..                       ")
-console.log("                   ..           ............ .    ,               ..            ")
-console.log("                    .   .         .. ...... . .....            ...              ")
-console.log("                            ................... ..             ..               ")
-console.log("                      ................,,,.... ..,             ..      ..        ")
-console.log("                 ......,............. ......   . .       .  .. .      .         ")
-console.log("              .. . .....  .....,...           . ,......     . ..                ")
-console.log("              . ..         . ......        ..   .,....   ......         ..      ")
-console.log("            . ..           ..  ..   ....  .,,,,..........     .  .. .. ..       ")
-console.log("              ..        .  . ... .   . ...,.. ............                      ")
-console.log("               .      .... ,......  . ........... . ....          . . ......    ")
-console.log("                   ..........  ....... .     . .  ..,...          .      .      ")
-console.log("              ......... ......  . ... .    .   ..       ..       ......         ")
-console.log("          . ..... .... ....      . .         .  .  ..     ..........   .        ")
-console.log("           ...........           . .           ... .   . ..... .. .             ")
-console.log("         .  ......        .       ..            .. .  .         . .             ")
-console.log("            .... .          . ......                .......                     ")
-console.log("                           . .......                 .  .                       ")
-console.log("                             ...   .                                            ")
-console.log("                              ...                                               ")
-console.log("                                .                                               ")
 
+fs.readFile(`${__dirname}/assets/askabor.txt`, function(err, data) {
+    if(err) throw err;
+    let arr = data.toString().replace(/\r\n/g,'\n').split('\n');
+    for(let i of arr) {
+        console.log(i);
+    }
+});
+
+//
