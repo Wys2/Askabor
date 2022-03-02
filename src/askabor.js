cb9f28804336f9687a26c57b2557ce7b7d9b784e
Wys2 <noahgaming951@gmail.com>
2022-03-02 21:32:29 +0100

    Ajout de puppeteer

--- src/askabor.js
+++ src/askabor.js
@@ -3,8 +3,7 @@
 // Variables globales
 const functions = require("./assets/functions.js")
 const fs = require('fs');
-const chalk = require("chalk")
-const pupeteer = require("puppeteer")
+const puppeteer = require("puppeteer")
 
 // Startup
 var args = process.argv.slice(2);
@@ -15,9 +14,31 @@ if(args[0] == "-t"){
 fs.readFile(`${__dirname}/assets/askabor.txt`, function(err, data) {
     if(err) throw err;
     let arr = data.toString().replace(/\r\n/g,'\n').split('\n');
-    for(let i of arr) {
-        console.log(i);
+    for(let e of arr) {
+        console.log(e);
     }
 });
 
-//
+//Start a new page
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.setUserAgent('Mozilla', {
+    architecture: 'Win64; x64',
+    mobile: false,
+    model: 'NT',
+    platform: 'Windows NT',
+    platformVersion: '10.0',
+  });
+
+  page.on('console', (msg) => {
+    if(args[0] == "-t") console.log('PAGE LOG:', msg.text());
+  })
+
+  await page.goto('https://tiktok.com', {
+    waitUntil: 'networkidle2',
+  });
+
+})();
+
